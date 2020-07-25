dc42f217f81138ec51959ec740989128b209eceb
tubone24 <tubo.yyyuuu@gmail.com>
2020-07-25 19:32:52 +0900

    :bug: Invalid search num

--- src/mergeRest.ts
+++ src/mergeRest.ts
@@ -6,33 +6,39 @@ export const mergeRest = (
   hotpepperRest: HotpepperRest[]
 ): GrunaviRest[] => {
   // ぐるなびAPIの結果の画像がしょぼいから画像をHotpepperAPIで置き換えて、かつぐるなびAPIの検索結果にないものを加える
+  let newGrunaviRest;
+  for (const hotpepperEntry of hotpepperRest) {
+    newGrunaviRest = changeList(grunaviRest, hotpepperEntry)
+  }
+  return newGrunaviRest;
+};
+
+const changeList = (grunaviRest, hotpepperEntry) => {
   const addNameList = grunaviRest.map(item => {
     return item.name;
   });
-  for (const hotpepperEntry of hotpepperRest) {
-    let include = false;
-    grunaviRest.forEach((grunaviItem) => {
-      if (grunaviItem.name === hotpepperEntry.name) {
-        grunaviItem.image_url.shop_image1 = hotpepperEntry.photo.pc.l;
-        include = true;
-      }
-    });
-    if (!include && !addNameList.includes(hotpepperEntry.name)) {
-      include = false;
-      grunaviRest.push({
-        name: hotpepperEntry.name,
-        image_url: {
-          shop_image1: hotpepperEntry.photo.pc.l
-        },
-        address: hotpepperEntry.address,
-        url: hotpepperEntry.urls.pc,
-        url_mobile: hotpepperEntry.urls.pc,
-        tel: '不明',
-        pr: {
-          pr_long: ''
-        }
-      })
+  let include = false;
+  grunaviRest.forEach((grunaviItem) => {
+    if (grunaviItem.name === hotpepperEntry.name) {
+      grunaviItem.image_url.shop_image1 = hotpepperEntry.photo.pc.l;
+      include = true;
     }
+  });
+  if (!include && !addNameList.includes(hotpepperEntry.name)) {
+    include = false;
+    grunaviRest.push({
+      name: hotpepperEntry.name,
+      image_url: {
+        shop_image1: hotpepperEntry.photo.pc.l
+      },
+      address: hotpepperEntry.address,
+      url: hotpepperEntry.urls.pc,
+      url_mobile: hotpepperEntry.urls.pc,
+      tel: '不明',
+      pr: {
+        pr_long: ''
+      }
+    })
   }
-  return grunaviRest;
-};
+  return grunaviRest
+}
