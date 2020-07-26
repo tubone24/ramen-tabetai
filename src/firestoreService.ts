5b7d60290a06dcd6da638437af1efddd1e98113d
tubone24 <tubo.yyyuuu@gmail.com>
2020-07-26 13:37:40 +0900

    :construction: Add Username change feature

--- src/firestoreService.ts
+++ src/firestoreService.ts
@@ -1,9 +1,10 @@
 export class FirestoreService {
   private firestore: any;
   constructor(email: string, key: string, projectId: string) {
-    console.log(key)
+    const replaceKey = key.replace(/REPLACE-LF-REPLACE/g, '\n');
+    console.log(replaceKey)
     // @ts-ignore because of Add GoogleScripts Library
-    this.firestore = FirestoreApp.getFirestore(email, key, projectId);
+    this.firestore = FirestoreApp.getFirestore(email, replaceKey, projectId);
   }
   updateData(collectionId: string, documentId: string, data) {
     this.firestore.updateDocument(collectionId + '/' + documentId, data, true);
